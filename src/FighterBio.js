a3e3da92734982b92b11728b46171e37114e1682
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-10-15 15:28:53 -0400

    something

--- src/FighterBio.js
+++ src/FighterBio.js
@@ -3,11 +3,14 @@ import "./Fighter.css";
 
 function FighterBio({ fighter }) {
   console.log(fighter);
-  {
-    fighter.map((elem) => {
-      return;
-    });
-  }
+
+  return (
+    <div>
+      <h1>{fighter[0].name}</h1>
+      <img src={fighter[0].image} alt={fighter[0].name} />
+      <p>{fighter[0].bio}</p>
+    </div>
+  );
 }
 
 export default FighterBio;
